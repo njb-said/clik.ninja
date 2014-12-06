cb45cdc1ef0bd789842c1dbb40023cab57d78738
njb_said <njb_said@hotmail.co.uk>
2014-12-06 18:33:10 +0000

    Clickable boxes
    I need a fancy design, I'll do that at some point

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
+AlertJS.setSetting("timeout", 3000);
 $(document).ready(function() {
-    var window.click = {};
-    click.status = 0;
-    var validArea = $('.clickable');
+    $('noscript').fadeOut("slow", function() {
+        $('.content').fadeIn();
+    });
 
-    $(validArea).on('click', function() {
+    var click = {};
+    click.status = 0;
+    click.clicks = 0;
+    click.time = 15;
+
+    $(document).on('click', function() {
         if(click.status == 0) { // Waiting
+            timer(click);
             click.status = 1;
             click.clicks = 1;
-        } else if(click.status == 1) { // Running
+            AlertJS.Notify.Top.Info("Get Clicking!", "You have 15 seconds to clicks as many times as you can!", true);
+        } else if(click.status == 1 && click.status > 0) { // Running
             click.clicks++;
-            console.log("New result is " + click.clicks);
         }
+        updatePage(click);
     });
 });
+
+function timer(app) {
+    if(app.time <= 0) {
+        app.status = 2;
+        AlertJS.Notify.Top.Info("Time Up!", "Your score: " + app.clicks, true);
+        $('#timer').fadeOut();
+    } else {
+        setTimeout(function() {
+            $('#timer').text(app.time);
+            app.time--;
+            timer(app);
+        }, 1000);
+    }
+};
+
+function updatePage(app) {
+    $('#clicks').text(app.clicks);
+};
