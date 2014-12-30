b8f0eda772bbc971563ae8ad652db7d18325f7f6
njb_said <njb_said@hotmail.co.uk>
2014-12-30 03:45:34 +0000

    Add submit form

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 AlertJS.setSetting("timeout", 3000);
 $(document).ready(function() {
     $('noscript').fadeOut("slow", function() {
+        $('div.attempt').show();
         $('.content').fadeIn();
     });
 
@@ -20,13 +21,30 @@ $(document).ready(function() {
         }
         updatePage(click);
     });
+
+    $('.save-score').on('click', function() {
+        if(!$('.save-score').prop('disabled')) {
+            $('.save-score').attr('disabled', 'disabled');
+            saveScore(click.clicks, $('input[name=username]').val());
+            console.log("Detected a click..");
+        }
+    });
 });
 
 function timer(app) {
+    if(app.time == 15) {
+        $('#timer-wrapper').fadeIn();
+        $('#timer').text(app.time);
+    }
     if(app.time <= 0) {
         app.status = 2;
         AlertJS.Notify.Top.Info("Time Up!", "Your score: " + app.clicks, true);
-        $('#timer').fadeOut();
+        $('div.attempt').fadeOut('slow', function() {
+            $('input[name=score]').val(app.clicks);
+            $('div.submit').fadeIn();
+            $('body').animate({backgroundColor: '#57B5CA'}, 'slow');
+            $('div.attempt').remove();
+        });
     } else {
         setTimeout(function() {
             $('#timer').text(app.time);
@@ -34,6 +52,40 @@ function timer(app) {
             timer(app);
         }, 1000);
     }
+};
+
+function saveScore(score, name) {
+    $.ajax({
+        url: "http://api.clik.ninja/scoresave",
+        type: "post",
+        timeout: 5000,
+        data: {
+            name: name,
+            score: score,
+            client: navigator.userAgent
+        },
+        success: function(data) {
+            if(data.status == 200) {
+                // Successful
+                console.log("INFO >> " + data.message);
+                // Would fade out and show leaderboard.
+            } else if(data.status == 400) {
+                // Bad Request
+                $('.save-score').removeAttr('disabled');
+                console.log("ERROR >> " + data.message);
+            } else {
+                // Error on server
+                $('.save-score').removeAttr('disabled');
+                console.log("SEVERE >> " + data.message);
+                AlertJS.Notify.Top.Error("Uh oh!", 'We were unable to save your score due to a technical error, sorry about that.');
+            }
+        },
+        error: function(xhr, opt, err) {
+            $('.save-score').removeAttr('disabled');
+            console.log("SEVERE >> " + err);
+            AlertJS.Notify.Top.Error("Uh oh!", 'We were unable to save your score due to a technical error, sorry about that.');
+        }
+    });
 };
 
 function updatePage(app) {
